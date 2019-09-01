296a04cc9050608efa35dc921308701f627c28d6
aruanto <aruanto@iu.edu>
2019-08-31 22:22:20 -0400

    Finished assignment

--- main.py
+++ main.py
@@ -5,4 +5,9 @@ import utils
 utils.check_version((3,7))
 utils.clear()
 
-print('Hello, Class!')
+print('Hi, Im Alex Ruanto)
+print('My favorite game right now is Destiny 2.')
+print('I have some concerns with my ability to keep up with the coding despite my prior experience.')
+print('Im excited to start more of my major classes')
+print('12004662')
+print('https://github.com/SatinVR')
